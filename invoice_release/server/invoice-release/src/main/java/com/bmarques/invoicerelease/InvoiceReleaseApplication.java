bcdcddf4b09a2213b6865f3e1c3080530773d280
Marques <tiagobm564@gmail.com>
2021-02-22 08:29:45 -0300

    Initial commit with BE project start

---
+++ invoice_release/server/invoice-release/src/main/java/com/bmarques/invoicerelease/InvoiceReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.bmarques.invoicerelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceReleaseApplication.class, args);
+	}
+
+}
---
+++ invoice_release/server/invoice-release/src/test/java/com/bmarques/invoicerelease/InvoiceReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmarques.invoicerelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
