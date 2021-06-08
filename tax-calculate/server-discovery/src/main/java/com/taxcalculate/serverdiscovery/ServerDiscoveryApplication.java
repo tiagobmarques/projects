23081415d632b880fdd4e6c77da6f2c783f01bc7
Tiago Marques <tiagobm564@gmail.com>
2021-06-08 14:10:10 -0300

    Added initial config to up Eureka Server

--- tax-calculate/server-discovery/src/main/java/com/taxcalculate/serverdiscovery/ServerDiscoveryApplication.java
+++ tax-calculate/server-discovery/src/main/java/com/taxcalculate/serverdiscovery/ServerDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.taxcalculate.serverdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServerDiscoveryApplication {
 
 	public static void main(String[] args) {
