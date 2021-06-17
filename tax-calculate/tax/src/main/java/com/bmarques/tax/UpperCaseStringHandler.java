c4f0ed028d6e5a47ae3a6b39cac1eab82c8a452b
Tiago Marques <tiagobm564@gmail.com>
2021-06-17 15:42:23 -0300

    Update JenkinsFile

--- tax-calculate/tax/src/main/java/com/bmarques/tax/UpperCaseStringHandler.java
+++ tax-calculate/tax/src/main/java/com/bmarques/tax/UpperCaseStringHandler.java
@@ -1,14 +1,25 @@
 package com.bmarques.tax;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.function.Function;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 @Component
-public class UpperCaseStringHandler implements Function<String, String> {
+@Slf4j
+public class UpperCaseStringHandler implements RequestStreamHandler {
 
     @Override
-    public String apply(String input) {
-        return input.toUpperCase();
+    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+        String inputStream = IOUtils.toString(input, StandardCharsets.UTF_8);
+        log.info("This is entrypoint!");
+        log.info(inputStream);
+        output.write(("Hello World - " + inputStream).getBytes());
     }
 }
